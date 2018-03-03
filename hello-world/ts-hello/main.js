9eb03a0272760ad57b51f7a16eaae1ddc04a3381
AndreFelipeCL <andrefelipecl@outlook.com>
2018-03-02 22:24:27 -0300

    Examples about how to use the assertions on TypeScript and get the IntelliSense

--- hello-world/ts-hello/main.js
+++ hello-world/ts-hello/main.js
@@ -1,30 +1,9 @@
-//Declaring variable of type number
-var a;
-//Declaring variable of type boolean
-var b;
-//Declaring variable of type String
-var c;
-//Declaring variable of type Any
-var d;
-//Declaring array variable of type Number
-var e = [1, 2, 3, 4];
-//Declaring array variable of type Any
-var f = ["1", 2, false];
-// Constants
-var ColorRed = 0;
-var ColorGreen = 1;
-var ColorBlue = 2;
-// Container for Constants (Enum)
-/**
- * This container sets the incremented value to declareted Enums:
- * Take a look in this verbose alternative declaration:
- *      enum Color { Red = 0, Green = 1, Blue = 2};
- */
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-;
-var backgroundColor = Color.Blue;
+// Type Assertions
+let message;
+message = 'abc';
+let isEndsWithC;
+let anotherWay;
+isEndsWithC = message.endsWith('c');
+anotherWay = message.endsWith('c');
+console.log("Assertion 1 - (<string>message) = " + isEndsWithC);
+console.log("Assertion 2 - (message as string) = " + anotherWay);
